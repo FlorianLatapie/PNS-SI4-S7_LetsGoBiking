1456e437f02231e87e661b2f059c636616b98b08
Florian Latapie <florian.latapie@etu.unice.fr>
2022-11-20 21:03:12 +0100

    création du proxy server

---
+++ C#/LetGoBikingCS/ConsoleApp_for_Self_Hosted_WS/IMyCalculatorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// add assembly System.ServiceModel  and using for the corresponding model
+using System.ServiceModel;
+
+namespace ConsoleApp_for_Self_Hosted_WS
+{
+
+    [ServiceContract()]
+    public interface ISimpleCalculator
+    {
+        [OperationContract()]
+        int Add(int num1, int num2);
+    }
+
+
+}
---
+++ C#/LetGoBikingCS/ConsoleApp_for_Self_Hosted_WS/MyCalculatorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_for_Self_Hosted_WS
+{
+    class SimpleCalculator : ISimpleCalculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+
+}
