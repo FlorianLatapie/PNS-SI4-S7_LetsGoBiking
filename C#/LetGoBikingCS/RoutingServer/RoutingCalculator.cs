fa1b637e0ab7a7258d9cc8011a1bbede8339c753
Florian Latapie <florian.latapie@etu.unice.fr>
2022-11-22 23:03:58 +0100

    ajout d'un tostring utilisant la reflexion

--- C#/LetGoBikingCS/RoutingServer/RoutingCalculator.cs
+++ C#/LetGoBikingCS/RoutingServer/RoutingCalculator.cs
@@ -1,4 +1,7 @@
-﻿using RoutingServer.ServiceReference1;
+﻿using System;
+using RoutingServer.ServiceReference1;
+using System.ComponentModel;
+using System.Text;
 
 namespace RoutingServer
 {
@@ -11,6 +14,29 @@ namespace RoutingServer
             _proxy = new APIJCDecauxProxyClient();
         }
 
+        private string ToString(object obj)
+        {
+            PropertyDescriptorCollection coll = TypeDescriptor.GetProperties(obj);
+            StringBuilder builder = new StringBuilder();
+            foreach (PropertyDescriptor pd in coll)
+            {
+                if (pd.GetValue(obj) == null)
+                {
+                    builder.Append($"{pd.Name} : null");
+                }
+                else if (pd.GetValue(obj).GetType().IsArray)
+                {
+                    builder.Append($"{pd.Name} : ");
+                    Array.ForEach((object[])pd.GetValue(obj), item => builder.Append($"{item.ToString()} "));
+                }
+                else
+                {
+                    builder.Append($"{pd.Name} : {pd.GetValue(obj)}");
+                }
+                builder.Append(Environment.NewLine);
+            }
+            return builder.ToString();
+        }
 
         public string GetItinerary(string origin, string destination)
         {
@@ -21,12 +47,11 @@ namespace RoutingServer
                 "Routing server : \n" +
                 "origin : " + origin + "\n" +
                 "destination : " + destination + "\n" +
-                "1 - contracts : " + contracts + "\n" +
-                "2 - stations : " + stationsOfFirstContract + "\n" +
-                "2 - stations[0] : " + stationsOfFirstContract[0] + "\n" +
-                "3 - one station : " + firstStation.ToString() + "\n" + 
-                "contracts[0].ToString() : " + contracts[0].ToString() + "\n" +
-                "contracts[0] : " + contracts[0] + "\n";
+                "1 - contracts : " + ToString(contracts) + "\n" +
+                "2 - stations : " + ToString(stationsOfFirstContract) + "\n" +
+                "2 - stations[0] : " + ToString(stationsOfFirstContract[0]) + "\n" +
+                "3 - one station : " + ToString(firstStation) + "\n" +
+                "contracts[0].ToString() : " + ToString(contracts[0]) + "\n";
             return res;
         }
     }
