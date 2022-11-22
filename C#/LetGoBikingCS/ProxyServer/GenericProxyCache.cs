fba1c44904bd52fc43c7a493ab8996732f87df53
Florian Latapie <florian.latapie@etu.unice.fr>
2022-11-22 23:38:02 +0100

    creation du cache dans le proxy

---
+++ C#/LetGoBikingCS/ProxyServer/GenericProxyCache.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+
+namespace ProxyServer
+{
+    public class GenericProxyCache<T> where T : class
+    {
+        private readonly DateTimeOffset _dtDefaultDateTimeOffset = ObjectCache.InfiniteAbsoluteExpiration;
+        private Dictionary<string, Tuple<T, DateTimeOffset>> _cache = new Dictionary<string, Tuple<T, DateTimeOffset>>();
+        
+        public T Get(string cacheItemName, DateTimeOffset dt)
+        {
+            UpdateCache();
+            
+            if (!_cache.ContainsKey(cacheItemName) || _cache[cacheItemName].Item1 == null)
+            {
+                _cache[cacheItemName] = new Tuple<T, DateTimeOffset>(Activator.CreateInstance<T>(), dt);
+            }
+
+            return _cache[cacheItemName].Item1;
+        }
+
+        public T Get(string cacheItemName, double dtSeconds)
+        {
+            return Get(cacheItemName, DateTimeOffset.Now.AddSeconds(dtSeconds));
+        }
+
+        public T Get(string cacheItemName)
+        {
+            return Get(cacheItemName, _dtDefaultDateTimeOffset);
+        }
+
+        private void UpdateCache()
+        {
+            //remove all expired items from the cache
+            _cache = this._cache
+                .Where(x => x.Value.Item2 > DateTime.Now)
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
