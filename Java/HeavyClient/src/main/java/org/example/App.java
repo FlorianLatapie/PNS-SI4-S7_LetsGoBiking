ad4d9fbe243536b8b8cc31e243f141e8bc201e14
Florian Latapie <florian.latapie@etu.unice.fr>
2022-11-28 21:17:45 +0100

    ajout de la map dans java

--- Java/HeavyClient/src/main/java/org/example/App.java
+++ Java/HeavyClient/src/main/java/org/example/App.java
@@ -4,9 +4,6 @@ import com.soap.ws.client.generated.RoutingCalculator;
 
 public class App {
     public static void main(String[] args) {
-        var watch = System.currentTimeMillis();
-        System.out.println("Start !");
-
         var server = new RoutingCalculator();
         var routingCalculator = server.getWSHttpBindingIRoutingCalculator();
         var res = routingCalculator.getItinerary("addr:place du général de gaulle rouen",
---
+++ Java/HeavyClient/src/main/java/org/example/waypoint/EventWaypoint.java
@@ -0,0 +1,6 @@
+package org.example.waypoint;
+
+public interface EventWaypoint {
+
+    public void selected(MyWaypoint waypoint);
+}
